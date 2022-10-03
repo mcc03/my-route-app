728665497278d1c686921732d32133d885a71c78
N00212147 <N00212147@iadt.ie>
2022-10-03 14:09:56 +0100

    first route app commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function aboutus()
+    {
+        return view('aboutus');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+}
